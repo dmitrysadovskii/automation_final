52ca98c780e961cb15a695550736e043192762cb
calambus <sergey.tomashov@gmail.com>
2021-03-14 18:57:30 +0300

    addd: allure

--- tests/test_group_from_db_displayed.py
+++ tests/test_group_from_db_displayed.py
@@ -1,6 +1,11 @@
 from pages.base_page import BAsePage
 from time import sleep
-def test_1(browser):
+import allure
+
+
+@allure.story("Check that main page opens")
+def test_base_page_opens(browser):
     bp = BAsePage(browser)
-    bp.open_base_page()
-    sleep(4)
+    with allure.step("Open main page"):
+        bp.open_base_page()
+        sleep(4)
