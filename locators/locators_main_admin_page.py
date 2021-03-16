5c3602c6d01323afc7d3e210be8d6f3e34d26e1f
dmitrysadovskii <48434081+dmitrysadovskii@users.noreply.github.com>
2021-03-16 21:37:40 +0300

    Update locators_main_admin_page.py

--- locators/locators_main_admin_page.py
+++ locators/locators_main_admin_page.py
@@ -2,6 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class MainAdminLOcators:
+    
+    def get_left_bar_item_locator(self, item):
+        return (By.XPATH, f"{item}")
     LOCATOR_GROUPS = (By.XPATH, "//a[@href='/admin/auth/group/']")
     LOCATOR_MAIN_TITLE = (By.XPATH, "//h1[contains(text(),'Site administration')]")
     LOCATOR_ADD_USER = (By.XPATH, "//a[@href='/admin/auth/user/add/']")
