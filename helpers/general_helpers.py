ad46c197821cfd66eabd5f52d7a58a84c23e0c40
dmitrysadovskii <48434081+dmitrysadovskii@users.noreply.github.com>
2021-03-16 21:00:20 +0300

    Update general_helpers.py

--- helpers/general_helpers.py
+++ helpers/general_helpers.py
@@ -6,7 +6,7 @@ def generate_string(symbols_count: int):
     gstring = ''.join(random.choices(ascii_lowercase + digits, k=symbols_count))
     return gstring
 
-
+# generate via randomint
 def generate_int(symbols_count: int):
     gstring = ''.join(random.choices(digits, k=symbols_count))
     return int(gstring)
