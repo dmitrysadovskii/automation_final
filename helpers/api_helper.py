7e180005f03d11d15034c740de30b9e961ccf4cc
dmitrysadovskii <48434081+dmitrysadovskii@users.noreply.github.com>
2021-03-16 21:12:43 +0300

    Update api_helper.py

--- helpers/api_helper.py
+++ helpers/api_helper.py
@@ -8,6 +8,12 @@ class Api:
     def __init__(self):
         self.base_url = 'https://petstore.swagger.io/v2/user/'
         self.headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        
+    def _assert_request(self, method, url, data=None, params=None, status_code=200):
+        response = requests(method, url)
+        assert response.status_code = status_code
+        return response.json()
+        
 
     def create_user(self, username: str, password: str):
         ids = generate_int(3)
@@ -21,9 +27,11 @@ class Api:
             "phone": "string",
             "userStatus": '0'
         }]
-        r = requests.post(self.base_url + 'createWithArray', data=json.dumps(data),
-                          headers=self.headers)
-        assert r.status_code == 200
+        url =
+        self._assert_request('POST', url, data=json.dump(data))
+#         r = requests.post(self.base_url + 'createWithArray', data=json.dumps(data),
+#                           headers=self.headers)
+#         assert r.status_code == 200
 
     def get_user_info(self, username: str):
         r = requests.get(self.base_url + username)
